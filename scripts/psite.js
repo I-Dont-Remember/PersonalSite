e4a1d93b92a29622696b2979e677068b0a5c03d0
I-Dont-Remember <kpquinn2@wisc.edu>
2017-01-11 21:57:46 -0600

    functional site done, still need more fancy js and hobbies section though

---
+++ scripts/psite.js
@@ -0,0 +1,9 @@
+function visitPage(page) {
+// This is ugly, I know, but I wanted to get it working and stop wasting time on a feature I can come back to at a later point
+  if (page == 2) {
+    window.open("https://github.com/I-Dont-Remember/PersonalSite");
+  } else if (page == 1){
+    window.open("https://github.com/I-Dont-Remember/WEsort");
+  }
+
+}
